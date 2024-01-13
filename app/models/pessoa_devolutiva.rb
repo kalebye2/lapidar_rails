613561c3e8f4d76af8535772e3b07d89a213c11c
kalebye2 <kalebye2@proton.me>
2024-01-13 13:13:56 -0300

    devolutiva

--- app/models/pessoa_devolutiva.rb
+++ app/models/pessoa_devolutiva.rb
@@ -1,5 +1,6 @@
 class PessoaDevolutiva < ApplicationRecord
   belongs_to :pessoa
+  alias paciente pessoa
   belongs_to :profissional
   belongs_to :responsavel, class_name: "Pessoa", foreign_key: :pessoa_responsavel_id
 
@@ -10,7 +11,13 @@ class PessoaDevolutiva < ApplicationRecord
     "#{pessoa.nome_abreviado} (#{p_responsavel}) - #{profissional.nome_abreviado}"
   end
 
-  def paciente
-    pessoa
+  def informacoes_por_extenso
+    p_responsavel = !responsavel.nil? ? " (#{responsavel.nome_abreviado})" : ""
+    "#{pessoa.nome_abreviado}#{p_responsavel} - #{data.strftime("%d/%m/%Y")} (#{profissional.descricao_completa})"
   end
+
+  def substituir_template_por_dados atributo=""
+    self[atributo]&.to_s&.gsub(/{%\s*(paciente|pessoa|cliente)\s*%}/, pessoa.nome)&.gsub(/{%\s*(responsavel|responsável|interessado|entrevistado)\s*%}/, responsavel.nome)
+  end
+
 end
