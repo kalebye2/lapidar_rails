c48f30a8551e9f1fd02d3f070e6fd181997c360d
kalebye2 <kalebye2@proton.me>
2024-01-16 02:09:25 -0300

    pasta vendor pesava muito, tirei

--- vendor/bundle/ruby/3.0.0/gems/simple_calendar-3.0.2/test/dummy/app/models/application_record.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationRecord < ActiveRecord::Base
-  if Rails.gem_version >= Gem::Version.new("7.0")
-    primary_abstract_class
-  else
-    self.abstract_class = true
-  end
-end
--- vendor/bundle/ruby/3.0.0/gems/simple_calendar-3.0.2/test/simple_calendar_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SimpleCalendarTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert SimpleCalendar::VERSION
-  end
-end
