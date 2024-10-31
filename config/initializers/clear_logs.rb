a762c80dc304cefd5901972a898e1cc864d94575
kalebye2 <kalebye2@proton.me>
2024-10-30 21:59:52 -0300

    adicionada limpeza de log a cada 2MB de log

---
+++ config/initializers/clear_logs.rb
@@ -0,0 +1,11 @@
+# clear logs
+
+if Rails.env.development?
+  MAX_LOG_SIZE = 2.megabytes
+
+  logs = File.join(Rails.root, 'log', '*.log')
+  if Dir[logs].any? { |log| File.size?(log).to_i > MAX_LOG_SIZE }
+    $stdout.puts "Running rake log:clear"
+    `rake log:clear`
+  end
+end
