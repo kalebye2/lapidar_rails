0623c5a54882c13e4dcf1c5ff8b187f7bf08b052
kalebye2 <kalebye2@proton.me>
2024-11-25 10:00:04 -0300

    ajuste de região do documento de integer pra string

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.time "horario_fim"
   end
 
-# Could not dump table "acompanhamento_horarios_temp" because of following StandardError
-#   Unknown type 'NUM' for column 'horario'
-
   create_table "acompanhamento_reajuste_motivos", force: :cascade do |t|
     t.string "motivo", limit: 100, null: false
   end
@@ -718,7 +715,7 @@ ActiveRecord::Schema[7.0].define(version: 0) do
   create_table "profissionais", force: :cascade do |t|
     t.integer "pessoa_id", null: false
     t.integer "profissional_funcao_id"
-    t.integer "documento_regiao_id"
+    t.string "documento_regiao_id", limit: 255
     t.string "documento_valor", limit: 255
     t.integer "desligado", default: 0
     t.text "bio"
