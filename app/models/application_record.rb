707ee46fe07dbd101fad004c28f0f755feb7e341
kalebye2 <kalebye2@proton.me>
2024-12-22 11:10:55 -0300

    adição de métodos que facilitam ver associações

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -7,6 +7,61 @@ class ApplicationRecord < ActiveRecord::Base
   
   MAX_TRIES_RANDOM = 3
 
+  def self.all_tables
+    self.base_class.connection.tables
+  end
+
+  # usar primeiras colunas como forma primária de mostrar o valor
+  def default_display
+    send(self.class.column_names[1])
+  end
+
+  def self.associations
+    reflect_on_all_associations
+  end
+
+  def self.association_names
+    associations.map { |association| [association.foreign_key, association.name] }.to_h
+  end
+
+  def self.association_urls
+    
+  end
+
+  def associations
+    self.class.associations.map { |association| self.send(association.name) }
+  end
+
+  def association_names
+    self.class.associations.map { |association| [association.foreign_key, association.name] }.to_h
+  end
+
+  def association_from_foreign_key key
+    self.send(association_names[key])
+  end
+
+  ["BelongsTo", "HasMany", "HasOne", "HasAndBelongsToMany", "Through"].each do |association_type|
+    define_singleton_method "#{association_type.underscore}_associations" do
+      associations.map { |association| association if association.class.to_s.include?(association_type) }.compact
+    end
+
+    define_singleton_method "#{association_type.underscore}_names" do
+      send("#{association_type.underscore}_associations").map { |association| association.name }
+    end
+
+    define_method "#{association_type.underscore}_associations" do
+      self.class.send("#{association_type.underscore}_names").map { |association_name| [association_name, send(association_name)] }.to_h
+    end
+  end
+
+  def self.foreign_keys
+    belongs_to_associations.map { |association| association.join_foreign_key }
+  end
+
+  def foreign_keys
+    self.class.foreign_keys
+  end
+
   @app_config = Rails.application.config
 
   before_create do
