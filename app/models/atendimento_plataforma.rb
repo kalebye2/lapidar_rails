6cf0ba691ae5d226bc12f44202f0f029eba93463
kalebye2 <kalebye2@proton.me>
2025-01-21 21:04:40 -0300

    pluralização e ajuste de relacionamentos

--- app/models/atendimento_plataforma.rb
+++ app/models/atendimento_plataforma.rb
@@ -1,3 +1,5 @@
 class AtendimentoPlataforma < ApplicationRecord
-  has_many :acompanhamento
+  has_many :acompanhamentos, foreign_key: :plataforma_id
+  has_many :atendimentos, through: :acompanhamentos
+  has_many :pessoas, through: :acompanhamentos
 end
